0ad76a39261ce95630fe7b3fcb4606dd93f0e3f4
shekhar maheswari <shekharmaheswari85@gmail.com>
2020-10-09 13:48:21 +0600

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,27 @@
+var restart =  document.querySelector('#b');
+
+var squares = document.querySelectorAll('td');
+
+function clearboard(){
+    for(var i=0;i<squares.length;i++){
+        squares[i].textContent = '';
+    }
+}
+
+restart.addEventListener('click',clearboard); 
+
+function changeMarker(){
+    if(this.textContent ===''){
+        this.textContent ='X';
+    }
+    else if(this.textContent ==='X'){
+        this.textContent ='O';
+    }
+    else{
+        this.textContent ='';
+    }
+}
+
+for (var index = 0; index < squares.length; index++) {
+    squares[index].addEventListener('click',changeMarker)
+}
